08874d7d36da8d906f1590d0ca56f4e2510af67a
Lassi Uosukainen <lassi.uosukainen@estiem.org>
2017-06-11 19:27:22 +0300

    Improved event-participants

--- src/components/events/event-participants.vue
+++ src/components/events/event-participants.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="event-apply">
-    <v-data-table v-bind:headers="headers" :items="eps" hide-actions class="elevation-1">
+    <v-data-table :loading="loading" no-data-text="No participants were found for this event" v-bind:headers="headers" :items="eps" hide-actions class="elevation-1" id="participants-table">
       <template slot="items" scope="props">
-        <td>{{ props.item.User.Name.Name }}</td>
-        <td>{{ props.item.User.LocalGroup.Name }}</td>
-        <td>{{ props.item.RegistrationStatus }}</td>
-        <td>{{ props.item.RegistrationDate }}</td>
+        <td>
+          <a :href="'https://www.estiem.org/Internal/default.aspx?PageId=833&mid=988&cmd=show&fp=TRUE&rpid=907&ids='+ props.item.User.Id">{{ props.item.User.Name.Name }}</a>
+        </td>
+        <td>
+          <a :href="'https://www.estiem.org/Internal/default.aspx?PageId=644&mid=668&cmd=show&fp=TRUE&rpid=907&ids='+ props.item.User.LocalGroup.Id">{{ props.item.User.LocalGroup.Name }}</a>
+        </td>
+        <td>{{ convertRegStatus(props.item.RegistrationStatus) }}</td>
+        <td>{{ props.item.RegistrationDate | formatdate }}</td>
       </template>
     </v-data-table>
   </div>
@@ -25,6 +29,7 @@ export default {
         { text: 'Status', value: 'RegistrationStatus' },
         { text: 'Application date', value: 'RegistrationDate' },
       ],
+      loading: true,
     }
   },
   computed: {
@@ -46,24 +51,64 @@ export default {
           this.eps = response.data;
           console.log(response);
           console.log(eps);
+          this.loading = false;
         });
+
     },
+    convertRegStatus(regstatus) {
+      if (regstatus == 0) {
+        return "Registered";
+      }
+      else if (regstatus == 1) {
+        return "Waiting";
+      }
+      else if (regstatus == 2) {
+        return "Canceled";
+      }
+      else if (regstatus == 3) {
+        return "Canceled after deadline";
+      }
+      else if (regstatus == 4) {
+        return "Did not show up";
+      }
+      else if (regstatus == 5) {
+        return "Organizer";
+      }
+      else if (regstatus == 6) {
+        return "Application withdrawn";
+      }
+      else {
+        return "Unknown";
+      }
+    }
   },
   created() {
+    let ckeditor = document.createElement('script');
     console.log("creatd");
+
+    ckeditor.setAttribute('src', "//cdn.ckeditor.com/4.6.2/full/ckeditor.js");
+    document.head.appendChild(ckeditor);
     this.fetchEventParticipants(this.eventid);
+  },
+  filters: {
+    formatdate(value) {
+      if (value) {
+        return moment(value).format('DD.MM.YYYY');
+      }
+    }
   },
 
 }
 </script>
 
 <style>
-#event-apply {
-  max-width: 800px;
-  margin: auto;
+#participants-table a {
+  text-decoration: none;
+  color: rgba(0, 0, 0, 0.87);
 }
 
-.checkbox {
-  margin: 0;
+#participants-table a:hover {
+  text-decoration: underline;
+  color: #205E44;
 }
 </style>
